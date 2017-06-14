00b8f33ef1e15c4f162da52d85a77f8130413e79
Dharini Murugaprabhu <dharinishree@gmail.com>
2017-06-14 11:12:40 -0700

    Added 5 piglatin conversion rules

--- piglatin.js
+++ piglatin.js
@@ -3,51 +3,131 @@ function PiglatinTranslator() {
   this.vowels = ['a','e','i','o','u'];
 }
 
-PiglatinTranslator.prototype.process = function(word) {
-  return word;
-};
-
+/* Start piglatin translations split index locations */
 PiglatinTranslator.prototype.beginWithVowel = function(word) {
-  return null;
+  if (this.vowels.includes(word[0])){
+      return 0;
+    }
 };
 
 PiglatinTranslator.prototype.schSinglePhoneme = function(word) {
-  return null;
+  if (word.slice(0, 3) == 'sch'){
+      return 3;
+  }
 };
 
 PiglatinTranslator.prototype.quSinglePhoneme = function(word) {
-  return null;
+  if (word.slice(0,2) == "qu"){
+      return 2;
+    }
 };
 
 PiglatinTranslator.prototype.beginWithConsonants = function(word) {
-  return null;
-};
-
-PiglatinTranslator.prototype.capitalize = function(word) {
-  return word.charAt(0).toUpperCase() + word.slice(1);
+  var words = word.split('');
+  for (i=0; i< words.length-1; i++){
+    if (this.vowels.includes(words[i])){
+      return i;
+    }
+  }
 };
 
 PiglatinTranslator.prototype.quAsConsonant = function(word) {
   var splits = word.split('qu');
-  console.log(splits);
   if (!this.vowels.includes(splits[0]) && splits[0].length == 1) {
     return 3;
   }
 };
 
-PiglatinTranslator.prototype.get_piglatin_by_index = function(word, index) {
-  return word;
+/* end */
+
+
+PiglatinTranslator.prototype.capitalize = function(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+PiglatinTranslator.prototype.constructPiglatinFromIndex = function(word, index) {
+  var left = word.slice(0, index);
+  var right = word.slice(index, word.length);
+  var rightPunctDel = 0;
+  var count = 0;
+  var pig_latin_word;
+
+  for (var i=right.length-1; i>0; i--){
+    if (right[i].charCodeAt(0)< 65 && right[i].charCodeAt(0) >122){
+      count +=1;
+    } else {
+      break;
+    }
+    rightPunctDel = count;
+  }
+  
+  if (word == this.capitalize(word)){
+
+    pig_latin_word = right[0].toUpperCase() +
+                      right.slice(1, ((right.length-1) - rightPunctDel)) +
+                      left[0].toLowerCase() + left.slice(1,(left.length-1)) +
+                      "ay" ;
+  } else if (rightPunctDel > 0) {
+    pig_latin_word = right.slice(0,(right.length-1) - right_punct_del) +
+                      left +
+                      "ay" + right.slice((right.length - right_punct_del), right.length-1);
+  } else {
+    pig_latin_word = right + left + "ay";
+  }
+
+  return pig_latin_word;
 };
 
 PiglatinTranslator.prototype.translate = function(words) {
-  return words;
+  var piglatinWords = [];
+  var wordsList = words.split(' ');
+  for (var i = 0; i <= wordsList.length-1; i++){
+    var pivotIndex = null;
+    var word = wordsList[i];
+
+    if (this.quAsConsonant(word)) {
+      pivotIndex = this.quAsConsonant(word);
+    }
+    else if (this.quSinglePhoneme(word)) {
+      pivotIndex = this.quSinglePhoneme(word);
+    }
+    else if (this.schSinglePhoneme(word)) {
+      pivotIndex = this.schSinglePhoneme(word);
+    }
+    else if (this.beginWithVowel(word)) {
+      pivotIndex = this.beginWithVowel(word);
+    }
+    else if (this.beginWithConsonants(word)) {
+      pivotIndex = this.beginWithConsonants(word);
+    }
+
+
+    piglatinWords.push(this.constructPiglatinFromIndex(word, pivotIndex));
+  }
+
+  var result =  piglatinWords.join(' ');
+  return result;
 };
 
 function convertToPiglatin(word) {
-  console.log("converting to piglatin")
   translator = new PiglatinTranslator();
   result = translator.translate(word);
 }
 
-convertToPiglatin("aquiet");
+convertToPiglatin("apple");
+convertToPiglatin("banana");
+convertToPiglatin("cherry");
+convertToPiglatin("eat pie");
+convertToPiglatin("three");
+convertToPiglatin("school");
+convertToPiglatin("quiet");
+convertToPiglatin("square");
+convertToPiglatin("the quick brown fox");
+convertToPiglatin("Square!");
+convertToPiglatin("hello");
+convertToPiglatin("hello world");
+convertToPiglatin("Hello world");
+convertToPiglatin("Hello, world!!");
+convertToPiglatin("eat apples");
+convertToPiglatin("quick brown fox");
 
